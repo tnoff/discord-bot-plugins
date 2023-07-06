6c24a08eb018e92e16488ca632fd5c9f209ebd65
tnoff <1326564+tnoff@users.noreply.github.com>
2023-07-05 21:35:15 -0700

    fix uncaught exception in download

--- music.py
+++ music.py
@@ -770,7 +770,7 @@ class DownloadClient():
         # Since we don't pass "url" directly anymore
         try:
             data = data['entries'][0]
-        except IndexError:
+        except (IndexError, TypeError):
             self.logger.warning(f'Music :: Error downloading youtube search "{source_dict["search_string"]}')
             return None
         except KeyError:
